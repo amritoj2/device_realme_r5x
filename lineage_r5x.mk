ddfae6d4d5215e294d175cd8b6bcc8cbeb3642c7
Amritorock <Amritokarmokar5i@gmail.com>
2022-07-21 15:07:39 +0600

    r5x : Init RiceDroid

--- lineage_r5x.mk
+++ lineage_r5x.mk
@@ -9,20 +9,24 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
 
-# Inherit some common PixelExperience stuff
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+# Inherit some common Ricedroid stuff
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Inherit from r5x device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
 
-TARGET_BOOT_ANIMATION_RES := 720
+# Boot Animation
+SUSHI_BOOTANIMATION := 720
+
 TARGET_GAPPS_ARCH := arm64
 TARGET_INCLUDE_LIVE_WALLPAPERS := false
+TARGET_FACE_UNLOCK_SUPPORTED := true
+TARGET_ENABLE_BLUR := true
 
 PRODUCT_BRAND := Realme
 PRODUCT_DEVICE := r5x
 PRODUCT_MANUFACTURER := Realme
-PRODUCT_NAME := aosp_r5x
+PRODUCT_NAME := lineage_r5x
 PRODUCT_MODEL := Realme 5 Series
 
 PRODUCT_GMS_CLIENTID_BASE := android-realme
@@ -33,3 +37,13 @@ TARGET_VENDOR_DEVICE_NAME := r5x
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME="r5x" \
     PRIVATE_BUILD_DESC="trinket-user 10 QKQ1.200209.002 release-keys"
+
+# RiceDroid Maintainer
+RICE_MAINTAINER := ⚡AMRITO⚡
+
+# Graphene Camera
+TARGET_BUILD_GRAPHENEOS_CAMERA := false
+
+# Pixel
+WITH_GMS := true
+
